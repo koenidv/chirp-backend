811a651c410fe0b50675750e61349e20437dbaf8
Florian König <koenidv@gmail.com>
2023-03-16 00:11:38 +0100

    :construction: chore(main.ts): add session middleware and cookie store This commit adds the session middleware and cookie store to the application. The session middleware is initialized using the `Session.initMiddleware()` method, and the cookie store is created using the `CookieStore` class. The `AppState` type is also defined to include the session object.

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 import { Application, Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+import {
+  CookieStore,
+  Session,
+} from "https://deno.land/x/oak_sessions@v4.1.0/mod.ts";
 import mockRouter from "./mock/MockRouter.ts";
 
-const router = new Router();
+export type AppState = {
+  session: Session;
+};
+
+const router = new Router<AppState>();
 router.get("/", (ctx) => {
   ctx.response.body = "Twitter Clone API available";
 });
@@ -9,7 +17,12 @@ router.get("/", (ctx) => {
 router.use("/mock", mockRouter.routes(), mockRouter.allowedMethods());
 
 const app = new Application();
+
+const store = new CookieStore("secretkeyorsmt");
+
+app.use(Session.initMiddleware());
 app.use(router.routes());
+
 app.addEventListener("listen", ({ hostname, port, secure }) => {
   console.log(
     `Listening on: ${secure ? "https://" : "http://"}${
