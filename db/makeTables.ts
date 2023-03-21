490fa4c55642d3e77c7d4af459ae6e343b9fd639
Florian König <koenidv@gmail.com>
2023-03-21 23:19:03 +0100

    Create tweets table

--- db/makeTables.ts
+++ db/makeTables.ts
@@ -5,5 +5,18 @@ client.queryArray`CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(24) NOT NULL,
     displayname VARCHAR(36) NOT NULL,
     passwordhash VARCHAR NOT NULL,
-    profile_url VARCHAR
+    profile_image_url VARCHAR,
+    PRIMARY KEY (user_id)
+);`;
+
+client.queryArray`CREATE TABLE IF NOT EXISTS tweets (
+    tweet_id BIGINT NOT NULL,
+    author_id INT NOT NULL,
+    content VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (tweet_id),
+    CONSTRAINT fk_author_id
+        FOREIGN KEY(author_id)
+            REFERENCES users(user_id)
+            ON DELETE CASCADE
 );`;
