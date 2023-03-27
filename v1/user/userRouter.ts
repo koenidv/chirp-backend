b43c001898ed2f01a0a8b2955a7c24bb68ca8c82
Florian König <koenidv@gmail.com>
2023-03-27 15:10:43 +0200

    Disallow getting  users without login

--- v1/user/userRouter.ts
+++ v1/user/userRouter.ts
@@ -40,6 +40,12 @@ userRouter.post("/", async (ctx) => {
 });
 
 userRouter.get("/:user_id", async (ctx) => {
+  if (!await ctx.state.session.get("user_id")) {
+    // user profiles are public, remove this check once we allow non-signed-in browsing
+    ctx.response.status = 401;
+    return;
+  }
+
   const user_id = ctx.params.user_id;
   if (!user_id) {
     ctx.response.status = 400;
