4ea78d22308acdb2a79f6ee0297c4bf7d5cbe5a3
Florian König <koenidv@gmail.com>
2023-03-29 12:20:08 +0200

    Improve username query

--- db/tweets.ts
+++ db/tweets.ts
@@ -33,10 +33,11 @@ export async function queryTweet(tweet_id: string): Promise<Tweet | false> {
   // all tweets are public, so no need to check if user is subscribed to author
 
   const tweet = await client.queryObject<Tweet>`
-  SELECT t.tweet_id, t.author_id, t.content, t.created_at, count(like_id), array_agg(m.user_id) AS mentions
+  SELECT t.tweet_id, t.author_id, t.content, t.created_at,
+  (SELECT COUNT(*) FROM likes WHERE likes.tweet_id = t.tweet_id) AS like_count,
+  (SELECT COUNT(*) FROM comments WHERE comments.tweet_id = t.tweet_id) AS comment_count,
+  (SELECT array_agg(m.user_id) FROM mentions as m WHERE t.tweet_id = m.tweet_id)
   FROM tweets as t
-      LEFT JOIN likes l on t.tweet_id = l.tweet_id
-      LEFT JOIN mentions m on t.tweet_id = m.tweet_id
   WHERE t.tweet_id = ${tweet_id}
   GROUP BY t.tweet_id, t.author_id, t.content, t.created_at`;
 
