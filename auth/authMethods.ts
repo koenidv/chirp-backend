f6cb2d3a4f40af1a6f5164e88d2e8a8b8b1a5f09
Florian König <koenidv@gmail.com>
2023-03-31 23:01:13 +0200

    Create authenticate function

--- auth/authMethods.ts
+++ auth/authMethods.ts
@@ -1,5 +1,6 @@
 import { email as emailvalidate } from "https://deno.land/x/validation@v0.4.0/mod.ts";
 import * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
+import { verify } from "https://deno.land/x/djwt@v2.2/mod.ts";
 
 export function validateEmailSchema(email: string): boolean {
   return emailvalidate.valid(email);
@@ -15,5 +16,20 @@ export async function comparePassword(
   password: string,
   hash: string,
 ): Promise<boolean> {
-  return await bcrypt.compareSync(password, hash);
+  return bcrypt.compareSync(password, hash);
+}
+
+export async function authenticate(ctx: any): Promise<string | false> {
+  const jwt = ctx.request.headers.get("Authorization")?.split(" ")[1];
+  if (!jwt) return false;
+  try {
+    const payload = await verify(
+      jwt,
+      Deno.env.get("JWT_KEY")!,
+      "HS512",
+    );
+    return payload?.user_id as string;
+  } catch (_e) {
+    return false;
+  }
 }
