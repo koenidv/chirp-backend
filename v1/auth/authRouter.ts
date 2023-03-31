ce59c92df290d0c776deea730a246de1c8b5f792
Florian König <koenidv@gmail.com>
2023-03-31 22:36:42 +0200

    Remove login and logout methods

--- v1/auth/authRouter.ts
+++ v1/auth/authRouter.ts
@@ -1,4 +1,5 @@
 import { Context, Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+import { create, verify } from "https://deno.land/x/djwt@v2.2/mod.ts"
 import {
   AUTH_ID_DUPLICATE_EMAIL,
   checkEmailAuth,
@@ -33,33 +34,19 @@ MFARouter.post("/register", async (ctx: Context) => {
     return;
   }
 
-  if (auth_id) ctx.state.session.set("auth_id", auth_id);
-
-  ctx.response.status = auth_id ? 200 : 400;
-});
-
-MFARouter.post("/login", async (ctx: Context) => {
-  const { email, password } = await ctx.request.body().value;
-
-  if (!email || !password) {
+  if (!auth_id) {
     ctx.response.status = 400;
     return;
   }
 
-  const check = await checkEmailAuth(email, password);
-
-  if (check) {
-    ctx.state.session.set("auth_id", check.auth_id);
-    ctx.state.session.set("user_id", check.user_id);
-    ctx.response.status = 200;
-  } else {
-    ctx.response.status = 401;
-  }
-});
-
-MFARouter.get("/logout", async (ctx: Context) => {
-  await ctx.state.session.deleteSession();
-  ctx.response.status = 200;
+  ctx.response.body = await create(
+    { alg: "HS512", typ: "JWT" },
+    {
+      auth_id: auth_id,
+      user_id: null,
+    },
+    Deno.env.get("JWT_KEY")!,
+  );
 });
 
 MFARouter.get("/whoami", async (ctx: Context) => {
