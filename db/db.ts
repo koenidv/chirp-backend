98e89a3b87bff190d5f5fd32db2d0dc807000854
Florian König <koenidv@gmail.com>
2023-04-26 19:20:04 +0200

    Add sanbox database flag

--- db/db.ts
+++ db/db.ts
@@ -1,9 +1,10 @@
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 import "https://deno.land/std@0.180.0/dotenv/load.ts";
 
-const client = new Client(Deno.env.get("DATABASE_URL"));
+const client = new Client(
+  Deno.env.get(Deno.args.includes("-s") ? "SANDBOX_DATABASE_URL" : "DATABASE_URL"),
+);
 
 export default client;
 
 await client.connect();
-  
