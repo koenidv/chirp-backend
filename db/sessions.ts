3a97cb7d20713745c215fdf9f65b6b63f363763e
Florian König <koenidv@gmail.com>
2023-11-03 13:31:35 +0100

    chore: return last session use in sessions response

--- db/sessions.ts
+++ db/sessions.ts
@@ -37,11 +37,13 @@ export async function sessionExists(session_id: string) {
   return result.rows[0].exists;
 }
 
-export async function getSessionsForUser(auth_id: string): Promise<{ session_id: string; auth_id: string; created_at: Date }[]> {
+export async function getSessionsForUser(auth_id: string): Promise<
+  { session_id: string, auth_id: string, last_ip: string, last_used: Date, created_at: Date }[]
+> {
   return await db(async (client) =>
     (await client.queryObject<
-      { session_id: string; auth_id: string; created_at: Date }
-    >`SELECT session_id, auth_id, created_at FROM sessions WHERE auth_id=${auth_id} ORDER BY last_used DESC`
+      { session_id: string, auth_id: string, last_ip: string, last_used: Date, created_at: Date }
+    >`SELECT session_id, auth_id, last_ip, last_used, created_at FROM sessions WHERE auth_id=${auth_id} ORDER BY last_used DESC`
     ).rows
   );
 }
